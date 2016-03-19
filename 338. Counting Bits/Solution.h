2c4fef225a2d5324fb4cb434345b2710f892e693
leo lin <linjunliang2010@hotmail.com>
2016-03-19 23:59:01 +0800

    338 version 3

--- 338. Counting Bits/Solution.h
+++ 338. Counting Bits/Solution.h
@@ -6,13 +6,8 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> result(num + 1);
-        int cur_power_of_2 = 1, next_power_of_2 = 2;
         for (int i = 1; i <= num; i++) {
-            if (i >= next_power_of_2) {
-                cur_power_of_2 = next_power_of_2;
-                next_power_of_2 <<= 1;
-            }
-            result[i] = result[i - cur_power_of_2] + 1;
+            result[i] = result[i >> 1] + (i & 0x01);
         }
         return result;
     }
