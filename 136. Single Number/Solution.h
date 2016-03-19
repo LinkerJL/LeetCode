7c48698a323b996e88664f9a53431263ec7434ff
leo lin <linjunliang2010@hotmail.com>
2016-03-20 02:57:37 +0800

    136 version 3

--- 136. Single Number/Solution.h
+++ 136. Single Number/Solution.h
@@ -1,26 +1,15 @@
 #pragma once
 #include <vector>
 using std::vector;
-#include <unordered_set>
-using std::unordered_set;
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> arise;
-        for (auto iter = nums.begin(); iter != nums.end(); iter++) {
-            auto found = arise.find(*iter);
-            if (found != arise.end()) {
-                arise.erase(found);
-            } else {
-                arise.insert(*iter);
-            }
+        int result = nums[0];
+        size_t len = nums.size();
+        for (size_t i = 1; i < len; i++) {
+            result ^= nums[i];
         }
-        auto left = arise.begin();
-        if (left == arise.end())
-        {
-            return 0;
-        }
-        return *left;
+        return result;
     }
 };
