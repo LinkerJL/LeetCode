507a34ac952cde3b4707674a7b3498d40ffd3f20
leo lin <linjunliang2010@hotmail.com>
2016-03-20 16:31:28 +0800

    283 version 3

--- 283. Move Zeroes/Solution.h
+++ 283. Move Zeroes/Solution.h
@@ -5,20 +5,15 @@ using std::vector;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int index = -1;
+        int j = 0;
         int len = nums.size();
         for (int i = 0; i < len; i++) {
-            if (nums[i] && i != index) {
-                if (index >= 0) {
-                    nums[index] = nums[i];
-                    nums[i] = 0;
-                    index++;
-                }
-            } else {
-                if (index < 0) {
-                    index = i;
-                }
+            if (nums[i]) {
+                nums[j++] = nums[i];
             }
+        }
+        while (j < len) {
+            nums[j++] = 0;
         }
     }
 };
