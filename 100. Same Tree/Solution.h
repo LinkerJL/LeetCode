325f72000cf469d4492e1427684c7eafec9f94ed
leo lin <linjunliang2010@hotmail.com>
2016-03-20 18:45:51 +0800

    100 version 2

--- 100. Same Tree/Solution.h
+++ 100. Same Tree/Solution.h
@@ -10,11 +10,8 @@ struct TreeNode {
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) {
-            return true;
-        }
-        if ((p && !q) || (!p && q)) {
-            return false;
+        if (!p || !q) {
+            return p == q;
         }
         if (p->val != q->val) {
             return false;
