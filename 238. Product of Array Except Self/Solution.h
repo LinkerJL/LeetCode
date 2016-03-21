bd2732a5f17ca7010abbcda54c80660c01b90e21
leo lin <linjunliang2010@hotmail.com>
2016-03-22 02:54:37 +0800

    238 version 2

--- 238. Product of Array Except Self/Solution.h
+++ 238. Product of Array Except Self/Solution.h
@@ -1,29 +1,19 @@
 #pragma once
 #include <vector>
 using std::vector;
-#include <numeric>
-using std::partial_sum;
-#include <functional>
-using std::multiplies;
 
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int len = nums.size();
-        int *forward_product = new int[len + 1];
-        int *backward_product = new int[len + 1];
-        forward_product[0] = 1;
-        partial_sum(nums.cbegin(), nums.cend(), forward_product + 1,
-            multiplies<int>());
-        backward_product[0] = 1;
-        partial_sum(nums.crbegin(), nums.crend(), backward_product + 1,
-            multiplies<int>());
-        vector<int> result(len);
+        vector<int> result(len, 1);
+        int product_from_begin = 1, product_from_end = 1;
         for (int i = 0; i < len; i++) {
-            result[i] = forward_product[i] * backward_product[len - 1 - i];
+            result[i] *= product_from_begin;
+            product_from_begin *= nums[i];
+            result[len - 1 - i] *= product_from_end;
+            product_from_end *= nums[len - 1 - i];
         }
-        delete[] forward_product;
-        delete[] backward_product;
         return result;
     }
 };
