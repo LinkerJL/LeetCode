2d63b45284e1ae5626302d2b5fe8751ea20314c0
leo lin <linjunliang2010@hotmail.com>
2016-03-25 01:13:00 +0800

    49 version 2

--- 49. Group Anagrams/Solution.h
+++ 49. Group Anagrams/Solution.h
@@ -13,8 +13,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> groups;
         for (auto str : strs) {
-            auto sorted_str = str;
-            sort(sorted_str.begin(), sorted_str.end());
+            auto sorted_str = StrSort(str);
             groups[sorted_str].push_back(str);
         }
         vector<vector<string>> result;
@@ -23,5 +22,23 @@ public:
             result.push_back(group.second);
         }
         return result;
+    }
+private:
+    string StrSort(string& str) {
+        const int letter_count = 26;
+        char frequency[letter_count] ={ 0 };
+        int len = str.size();
+        for (int i = 0; i < len; i++) {
+            frequency[str[i] - 'a']++;
+        }
+        string sorted_str(len, 'a');
+        int index = 0;
+        for (int i = 0; i < letter_count; i++) {
+            for (int j = 0; j < frequency[i]; j++) {
+                sorted_str[index] += i;
+                index++;
+            }
+        }
+        return sorted_str;
     }
 };
