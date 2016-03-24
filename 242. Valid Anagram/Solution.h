04dbbd5f456c3d3c102e78273c78bf14dc58c6a7
leo lin <linjunliang2010@hotmail.com>
2016-03-25 01:30:55 +0800

    242 version 2

--- 242. Valid Anagram/Solution.h
+++ 242. Valid Anagram/Solution.h
@@ -7,8 +7,21 @@ using std::sort;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        const int letter_count = 26;
+        int balance[letter_count] ={ 0 };
+        int len = s.size();
+        if (len != t.size()) {
+            return false;
+        }
+        for (int i = 0; i < len; i++) {
+            balance[s[i] - 'a']++;
+            balance[t[i] - 'a']--;
+        }
+        for (int i = 0; i < letter_count; i++) {
+            if (balance[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 };
