7f3b859c7550e249d8364b5ce6ecaddae7da2c19
leo lin <linjunliang2010@hotmail.com>
2016-03-26 20:18:54 +0800

    326 version 3

--- 326. Power of Three/Solution.h
+++ 326. Power of Three/Solution.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <cmath>
+#include <climits>
 
 class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n > 1) {
-            static const int max_3_num = pow(3, 19);
+            static const int max_power = log(INT_MAX) / log(3);
+            static const int max_3_num = pow(3, max_power);
             return (max_3_num % n == 0);
         }
         return n == 1;
