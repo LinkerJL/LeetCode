58b2aa542007d0cf44c11e0ec853b196a9937bf4
leo lin <linjunliang2010@hotmail.com>
2016-03-26 20:07:31 +0800

    325 version 1

---
+++ 326. Power of Three/Solution.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        return (n % 3) == 0 && isPowerOfThree(n / 3);
+    }
+};
