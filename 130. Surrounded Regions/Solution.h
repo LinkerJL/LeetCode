55422d5b64e06c58476c660ff81f6621254a2a81
leo lin <linjunliang2010@hotmail.com>
2016-03-27 03:37:42 +0800

    130 version 2

--- 130. Surrounded Regions/Solution.h
+++ 130. Surrounded Regions/Solution.h
@@ -1,42 +1,42 @@
 #pragma once
 #include <vector>
 using std::vector;
-#include <queue>
-using std::queue;
 #include <utility>
 using std::pair;
 
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int height = board.size();
-        if (!height) {
+        height_ = board.size();
+        if (!height_) {
             return;
         }
-        int width = board[0].size();
-        if (!width) {
+        width_ = board[0].size();
+        if (!width_) {
             return;
         }
-        queue<Pos> q;
         // top & bottom
-        for (int i = 0; i < width; i++) {
+        for (int i = 0; i < width_; i++) {
             if (board[0][i] == O) {
-                q.push({ 0, i });
+                board[0][i] = B;
+                depthFirstSearch(board, { 0, i });
             }
-            if (board[height - 1][i] == O) {
-                q.push({ height - 1, i });
+            if (board[height_ - 1][i] == O) {
+                board[height_ - 1][i] = B;
+                depthFirstSearch(board, { height_ - 1, i });
             }
         }
         // left & right
-        for (int i = 1; i < height - 1; i++) {
+        for (int i = 1; i < height_ - 1; i++) {
             if (board[i][0] == O) {
-                q.push({ i, 0 });
+                board[i][0] = B;
+                depthFirstSearch(board, { i, 0 });
             }
-            if (board[i][width - 1] == O) {
-                q.push({ i, width - 1 });
+            if (board[i][width_ - 1] == O) {
+                board[i][width_ - 1] = B;
+                depthFirstSearch(board, { i, width_ - 1 });
             }
         }
-        breadthFirstSearch(board, q);
         for (auto& row : board) {
             for (auto& point : row) {
                 if (point == O) {
@@ -50,24 +50,19 @@ public:
 private:
     typedef pair<int, int> Pos;
     enum { O='O', X='X', B='B' };
-    static void breadthFirstSearch(vector<vector<char>>& board, queue<Pos>& q) {
-        static const Pos move[4] ={ {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-        int height = board.size();
-        int width = board[0].size();
-        while (!q.empty()) {
-            Pos p = q.front();
-            q.pop();
-            board[p.first][p.second] = B;
-            for (auto& m : move) {
-                Pos new_p(p.first + m.first, p.second + m.second);
-                // ignore the edges
-                if (new_p.first <= 0 || new_p.first >= height - 1 ||
-                    new_p.second <= 0 || new_p.second >= width - 1) {
-                    continue;
-                }
-                if (board[new_p.first][new_p.second] == O) {
-                    q.push(new_p);
-                }
+    int height_;
+    int width_;
+    void depthFirstSearch(vector<vector<char>>& board, Pos p) {
+        static const Pos move[4] ={ { -1, 0 },{ 1, 0 },{ 0, -1 },{ 0, 1 } };
+        for (auto& m : move) {
+            Pos new_p(p.first + m.first, p.second + m.second);
+            if (new_p.first <= 0 || new_p.first >= height_ - 1 ||
+                new_p.second <= 0 || new_p.second >= width_ - 1) {
+                continue;
+            }
+            if (board[new_p.first][new_p.second] == O) {
+                board[new_p.first][new_p.second] = B;
+                depthFirstSearch(board, new_p);
             }
         }
     }
