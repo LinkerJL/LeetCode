1230fda74a6ddaab92d775326792b7732b2ed956
leo lin <linjunliang2010@hotmail.com>
2016-03-27 18:22:01 +0800

    169 version 5

--- 169. Majority Element/Solution.h
+++ 169. Majority Element/Solution.h
@@ -1,25 +1,25 @@
 #pragma once
 #include <vector>
 using std::vector;
+#include <cstdlib>
+#include <ctime>
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        const int byte_bits = 8;
-        int major = 0;
         int len = nums.size();
-        for (int i = 0; i < sizeof(int) * byte_bits; i++) {
-            int mask = 1 << i, bit_counts = 0;
+        srand((unsigned int)time(nullptr));
+        while (true) {
+            int candidate = nums[rand() % len];
+            int count = 0;
             for (int n : nums) {
-                if (n & mask) {
-                    bit_counts++;
-                    if (bit_counts > len / 2) {
-                        major |= mask;
-                        break;
+                if (candidate == n) {
+                    count++;
+                    if (count > len / 2) {
+                        return candidate;
                     }
                 }
             }
         }
-        return major;
     }
 };
