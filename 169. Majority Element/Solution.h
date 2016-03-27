924724a3674200ebbfa09aa7d9e94531060cbefc
leo lin <linjunliang2010@hotmail.com>
2016-03-27 18:04:49 +0800

    169 version 3

--- 169. Majority Element/Solution.h
+++ 169. Majority Element/Solution.h
@@ -1,13 +1,26 @@
 #pragma once
 #include <vector>
 using std::vector;
-#include <algorithm>
-using std::sort;
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        //! Moore Voting Algorithm
+        // Basic idea is if we cancel out each occurrence of an element e with
+        // all the other elements that are different from e than e will exist
+        // till end if it is a majority element.
+        int major, counts = 0;
+        int len = nums.size();
+        for (int n : nums) {
+            if (!counts) {
+                major = n;
+                counts = 1;
+            } else if (major == n) {
+                counts += 1;
+            } else {
+                counts -= 1;
+            }
+        }
+        return major;
     }
 };
