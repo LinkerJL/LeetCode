940b738a717f5c6e760e43c83562d12b3f9ee8d1
leo lin <linjunliang2010@hotmail.com>
2016-04-02 04:06:24 +0800

    290 version 2

--- 290. Word Pattern/Solution.h
+++ 290. Word Pattern/Solution.h
@@ -3,39 +3,24 @@
 using std::string;
 #include <unordered_map>
 using std::unordered_map;
+#include <sstream>
+using std::istringstream;
 
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
-        unordered_map<char, string> pattern_to_str;
-        unordered_map<string, char> str_to_pattern;
+        int pattern2index[26] ={ 0 };
+        unordered_map<string, int> str2index;
+        istringstream in(str);
+        int index = 0;
         int pattern_len = pattern.size();
-        int str_len = str.size();
-        if (!pattern_len && str_len) {
-            return false;
-        }
-        for (int i = 0, k = 0; i < pattern_len; i++, k++) {
-            int begin = k;
-            while (k < str_len && str[k] != ' ') {
-                k++;
-            }
-            if ((i == pattern_len - 1) ^ (k == str_len)) {
+        for (string word; in >> word; index++) {
+            int key = pattern[index] - 'a';
+            if (index == pattern_len || pattern2index[key] != str2index[word]) {
                 return false;
             }
-            string sub = str.substr(begin, k - begin);
-            auto it1 = pattern_to_str.find(pattern[i]);
-            auto it2 = str_to_pattern.find(sub);
-            if (it1 == pattern_to_str.end() && it2 == str_to_pattern.end()) {
-                pattern_to_str[pattern[i]] = sub;
-                str_to_pattern[sub] = pattern[i];
-            } else if (it1 != pattern_to_str.end() && it2 != str_to_pattern.end()) {
-                if (it1->second != sub) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+            pattern2index[key] = str2index[word] = index + 1;
         }
-        return true;
+        return index == pattern_len;
     }
 };
