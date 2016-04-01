36e128aee03acec0ef738615708e96aa6cf560b1
leo lin <linjunliang2010@hotmail.com>
2016-04-02 01:18:55 +0800

    60 version 2

--- 60. Permutation Sequence/Solution.h
+++ 60. Permutation Sequence/Solution.h
@@ -6,22 +6,20 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         static int factorial[] ={ 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
-        int m[9] ={ 0 };
+        string result(n, '1');
+        for (int i = 1; i < n; i++) {
+            result[i] += i;
+        }
         k--;
-        for (int i = n - 1; i >= 1; i--) {
-            m[i] = k / factorial[i];
-            k = k % factorial[i];
-        }
-        string result;
-        for (int i = 0; i < n; i++) {
-            result += i + '1';
-        }
         for (int i = 0; i < n - 1; i++) {
-            int pos = m[n - 1 - i];
+            int pos = k / factorial[n - 1 - i];
+            k = k % factorial[n - 1 - i];
             if (pos) {
                 char c = result[pos + i];
-                result.erase(pos + i, 1);
-                result.insert(i, 1, c);
+                for (int j = pos + i; j > i; j--) {
+                    result[j] = result[j - 1];
+                }
+                result[i] = c;
             }
         }
         return result;
