ef2ad44ca73f52da38a5a923d7c599020a3197d7
leo lin <linjunliang2010@hotmail.com>
2016-04-06 05:50:49 +0800

    278 version 1

---
+++ 278. First Bad Version/Solution.h
@@ -0,0 +1,22 @@
+#pragma once
+
+bool isBadVersion(int version) {
+    return true;
+}
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left != right) {
+            // avoid overflow
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
