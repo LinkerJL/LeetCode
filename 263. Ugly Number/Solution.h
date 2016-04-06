1d60a7d946a770df4b6937d90e17f42c1323815e
leo lin <linjunliang2010@hotmail.com>
2016-04-07 02:15:15 +0800

    263 version 2

--- 263. Ugly Number/Solution.h
+++ 263. Ugly Number/Solution.h
@@ -3,18 +3,13 @@
 class Solution {
 public:
     bool isUgly(int num) {
-        static int ugly_num[] ={ 2, 3, 5 };
-        if (num <= 0) {
-            return false;
-        }
-        if (num == 1) {
-            return true;
-        }
-        for (int i = 0; i < sizeof(ugly_num) / sizeof(int); i++) {
-            if (!(num % ugly_num[i])) {
-                return isUgly(num / ugly_num[i]);
+        if (num > 0) {
+            for (int i = 2; i < 6; i++) {
+                while (num % i == 0) {
+                    num /= i;
+                }
             }
         }
-        return false;
+        return num == 1;
     }
 };
