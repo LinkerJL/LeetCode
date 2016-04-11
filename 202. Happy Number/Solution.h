565bf8cce5499cd5db194a0fb66d82fc446c93af
leo lin <linjunliang2010@hotmail.com>
2016-04-11 13:16:36 +0800

    202 version 4

--- 202. Happy Number/Solution.h
+++ 202. Happy Number/Solution.h
@@ -3,13 +3,19 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        // Floyd's Cycle Detection Algorithm
-        int slow = n, fast = n;
-        do {
-            slow = digitSquareSum(slow);
-            fast = digitSquareSum(digitSquareSum(fast));
-        } while (fast != 1 && slow != fast);
-        return fast == 1;
+        // Brent's Cycle Detection Algorithm
+        int move = n, stay = 0;
+        int step = 0, interval = 2;
+        while (move != 1 && move != stay) {
+            if (step == interval) {
+                stay = move;
+                interval *= 2;
+                step = 0;
+            }
+            move = digitSquareSum(move);
+            step += 1;
+        }
+        return move == 1;
     }
 private:
     int digitSquareSum(int n) {
