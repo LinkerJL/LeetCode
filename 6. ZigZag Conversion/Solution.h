2a050aa4bf55468fddd442b86bc59e580c90d825
leo lin <linjunliang2010@hotmail.com>
2016-04-15 01:36:44 +0800

    6 version 2

--- 6. ZigZag Conversion/Solution.h
+++ 6. ZigZag Conversion/Solution.h
@@ -1,38 +1,35 @@
 #pragma once
 #include <string>
 using std::string;
-#include <vector>
-using std::vector;
+#include <memory>
+using std::unique_ptr;
 #include <numeric>
 using std::accumulate;
 
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<string> ss(numRows);
+        if (numRows <= 1) {
+            return s;
+        }
+        auto array = unique_ptr<int[]>(new int[2 * numRows - 2]);
+        for (auto i = 0; i < numRows; ++i) {
+            array[i] = i;
+        }
+        for (auto i = numRows; i < 2 * numRows - 2; ++i) {
+            array[i] = 2 * numRows - 2 - i;
+        }
+        auto ss = unique_ptr<string[]>(new string[numRows]);
         auto len = s.size();
-        auto func = [&](int& i, int j) -> bool {
-            ss[j].push_back(s[i]);
-            ++i;
-            return i >= len;
-        };
-        auto down = true;
         for (auto i = 0; i < len; ) {
-            if (down) {
-                for (auto j = 0; j < numRows; ++j) {
-                    if (func(i, j)) {
-                        break;
-                    }
-                }
-            } else {
-                for (auto j = numRows - 2; j > 0; --j) {
-                    if (func(i, j)) {
-                        break;
-                    }
+            for (auto j = 0; j < 2 * numRows - 2; ++j) {
+                ss[array[j]].push_back(s[i]);
+                ++i;
+                if (i >= len) {
+                    break;
                 }
             }
-            down = !down;
         }
-        return accumulate(ss.begin(), ss.end(), string(""));
+        return accumulate(&ss[0], &ss[numRows], string(""));
     }
 };
