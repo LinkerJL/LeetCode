6a074bd7d873b944c1587b7bc275680fca20b21d
leo lin <linjunliang2010@hotmail.com>
2016-04-19 02:28:38 +0800

    8 version 2

--- 8. String to Integer (atoi)/Solution.h
+++ 8. String to Integer (atoi)/Solution.h
@@ -2,33 +2,27 @@
 #include <string>
 using std::string;
 #include <climits>
+#include <cctype>
 
 class Solution {
 public:
     int myAtoi(string str) {
-        auto sign = 0;
+        auto i = 0;
+        while (isspace(str[i])) {
+            ++i;
+        }
+        auto sign = 1;
+        if (str[i] == '+' || str[i] == '-') {
+            sign = (str[i] == '+') ? 1 : -1;
+            ++i;
+        }
         auto num = 0LL;
-        for (auto c : str) {
-            if (c == ' ' || c == '\t') {
-                if (sign) {
-                    break;
-                }
-            } else if (c == '+' || c == '-') {
-                if (sign) {
-                    break;
-                }
-                sign = (c == '+') ? 1 : -1;
-            } else if ('0' <= c && c <= '9') {
-                if (!sign) {
-                    sign = 1;
-                }
-                num = 10 * num + c - '0';
-                if (sign * num < INT_MIN || sign * num > INT_MAX) {
-                    return (sign > 0) ? INT_MAX : INT_MIN;
-                }
-            } else {
-                break;
+        while (isdigit(str[i])) {
+            num = 10 * num + str[i] - '0';
+            if (sign * num < INT_MIN || sign * num > INT_MAX) {
+                return (sign > 0) ? INT_MAX : INT_MIN;
             }
+            ++i;
         }
         return sign * num;
     }
