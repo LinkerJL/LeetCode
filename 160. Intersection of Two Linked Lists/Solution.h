c0324a82fa47d5b2f20b189afada0efa18aa4edc
leo lin <linjunliang2010@hotmail.com>
2016-04-26 21:53:42 +0800

    160 verson 3

--- 160. Intersection of Two Linked Lists/Solution.h
+++ 160. Intersection of Two Linked Lists/Solution.h
@@ -9,22 +9,10 @@ struct ListNode {
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (!(headA && headB)) {
-            return nullptr;
-        }
         auto node_a = headA, node_b = headB;
         while (node_a != node_b) {
-            node_a = node_a->next;
-            node_b = node_b->next;
-            if (node_a == node_b) {
-                return node_a;
-            }
-            if (!node_a) {
-                node_a = headB;
-            }
-            if (!node_b) {
-                node_b = headA;
-            }
+            node_a = node_a ? node_a->next : headB;
+            node_b = node_b ? node_b->next : headA;
         }
         return node_a;
     }
