fa4fec80af869fff75dd39c738ecedf7ed296fa5
leo lin <linjunliang2010@hotmail.com>
2016-04-28 22:51:45 +0800

    299 version 3

--- 299. Bulls and Cows/Solution.h
+++ 299. Bulls and Cows/Solution.h
@@ -9,26 +9,17 @@ public:
         //assert(secret.length() == guess.length());
         const int kRange = 10;
         auto As = 0, Bs = 0;
-        int secret_num[kRange] = { 0 };
-        int guess_num[kRange] = { 0 };
+        int cache[kRange] = { 0 };
         auto len = secret.length();
         for (auto i = 0U; i < len; i++) {
             if (secret[i] == guess[i]) {
                 As++;
                 continue;
             }
-            if (guess_num[secret[i] - '0'] > 0) {
-                guess_num[secret[i] - '0']--;
-                Bs++;
-            } else {
-                secret_num[secret[i] - '0']++;
-            }
-            if (secret_num[guess[i] - '0'] > 0) {
-                secret_num[guess[i] - '0']--;
-                Bs++;
-            } else {
-                guess_num[guess[i] - '0']++;
-            }
+            Bs += (cache[secret[i] - '0'] > 0);
+            cache[secret[i] - '0']--;
+            Bs += (cache[guess[i] - '0'] < 0);
+            cache[guess[i] - '0']++;
         }
         return to_string(As) + 'A' + to_string(Bs) + 'B';
     }
