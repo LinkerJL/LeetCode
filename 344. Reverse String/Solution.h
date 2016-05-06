9e340c5b04c95ebc1ad6553223b47e658a4e201f
leo lin <linjunliang2010@hotmail.com>
2016-05-06 20:01:42 +0800

    344 version 2

--- 344. Reverse String/Solution.h
+++ 344. Reverse String/Solution.h
@@ -3,13 +3,16 @@
 using std::string;
 using std::begin;
 using std::end;
-#include <algorithm>
-using std::reverse;
+using std::iter_swap;
 
 class Solution {
 public:
     string reverseString(string s) {
-        reverse(begin(s), end(s));
+        auto begin_iter = begin(s);
+        auto end_iter = end(s);
+        while (begin_iter != end_iter && begin_iter != --end_iter) {
+            iter_swap(begin_iter++, end_iter);
+        }
         return s;
     }
 };
