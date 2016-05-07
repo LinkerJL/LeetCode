ea038765262c93fb1b0443d1ba501c153c1b655b
leo lin <linjunliang2010@hotmail.com>
2016-05-08 06:40:13 +0800

    189 version 5

--- 189. Rotate Array/Solution.h
+++ 189. Rotate Array/Solution.h
@@ -1,17 +1,22 @@
 #pragma once
 #include <vector>
 using std::vector;
-using std::reverse;
+using std::swap;
 
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         auto len = nums.size();
-        if (len == 0 || (k %= len) == 0) {
+        if (len < 1 || (k %= len) == 0) {
             return;
         }
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        auto rotated = 0U;
+        for (auto i = 0U; rotated < len; ++i) {
+            for (auto j = (i + k) % len; j != i; j = (j + k) % len) {
+                swap(nums[i], nums[j]);
+                ++rotated;
+            }
+            ++rotated;
+        }
     }
 };
